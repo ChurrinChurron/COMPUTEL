238129e3ac9632587c8faddef6afad4b216260eb
ChurrinChurron <marianafantasy8@gmail.com>
2023-05-04 11:23:15 -0400

    COMPUTEL avance 2

--- src/js/splide.js
+++ src/js/splide.js
@@ -1,15 +1,19 @@
+/*SLIDER MARCAS*/
+
 var splide = new Splide( '#sliderMarcas', {
     type   : 'loop',
     perPage: 3,
     perMove: 1,
   } );
   
-  splide.mount();
+splide.mount();
 
-  var splide = new Splide( '#sliderProduc', {
+  /*SLIDER PRODUCTOS*/
+
+var splide = new Splide( '#sliderProduc', {
     type   : 'loop',
     perPage: 3,
     perMove: 1,
-  } );
+} );
   
-  splide.mount();
+splide.mount();
